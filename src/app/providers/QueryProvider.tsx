5af0393914a8bbc0f4e655724719a13266ae6636
Nguyen Huynh Hoang Uyen <hoanguyen91011@gmail.com>
2026-08-06 20:55:29 +0700

    push loi lai ne

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -6,7 +6,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools tttt initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
